68d1bdd4c653f289f87a5e7bb952849b6f7a69bf
João Paulo Kuboszewski Castilho <joao.pkc@gmail.com>
2018-11-21 18:34:10 -0200

    Versão1 usando mutex.

--- mutex_sem/mutex_sem.ino
+++ mutex_sem/mutex_sem.ino
@@ -10,6 +10,7 @@ static void customer(void *pvParameters);
 SemaphoreHandle_t semBarber;
 SemaphoreHandle_t semCustomers;
 SemaphoreHandle_t mutexLugares;
+SemaphoreHandle_t mutexImprime;
 /* Declaração da variável de lugares disponíveis */
 int lugares = 5;
 
@@ -20,20 +21,63 @@ void setup() {
   vSemaphoreCreateBinary(semBarber);
   semCustomers = xSemaphoreCreateCounting(5, 0);
   mutexLugares = xSemaphoreCreateMutex();
+  mutexImprime = xSemaphoreCreateMutex();
 
   /* Se está tudo ok com os semáforos, vamos criar as tarefas */
-  if (semBarber != NULL && semCustomers != NULL && mutexLugares != NULL) {
+  if (semBarber != NULL && semCustomers != NULL && mutexLugares != NULL
+      && mutexImprime != NULL) {
     xTaskCreate(barber, "Barber", 200, NULL, 1, NULL);
     xTaskCreate(customer, "Customer", 200, NULL, 1, NULL);
+    vTaskStartScheduler();
   }
+  for( ; ; );
+  return 0;
 }
 
 static void barber(void *pvParameters) {
-  
+  while (true) {
+    xSemaphoreTake(semCustomers, portMAX_DELAY);
+    xSemaphoreTake(mutexImprime, portMAX_DELAY);
+      Serial.println("Barbeiro pegou cliente");
+      Serial.flush();
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+    xSemaphoreGive(mutexImprime);
+    xSemaphoreTake(mutexLugares, portMAX_DELAY);
+      lugares++;
+      
+      xSemaphoreTake(semBarber, portMAX_DELAY);
+      
+    xSemaphoreGive(mutexLugares);
+    xSemaphoreTake(mutexImprime, portMAX_DELAY);
+        Serial.println("Cortando Cabelo");
+        Serial.flush();
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+      xSemaphoreGive(mutexImprime);
+      xSemaphoreGive(semBarber);
+    
+    
+  }
 }
 
 static void customer(void *pvParameters) {
-  
+  while (true) {
+    xSemaphoreTake(mutexLugares, portMAX_DELAY);
+      if (lugares > 0) {
+        lugares--;
+
+          xSemaphoreTake(mutexImprime, portMAX_DELAY);
+            Serial.println("Chegou um novo cliente");
+            Serial.flush();
+            vTaskDelay(3000 / portTICK_PERIOD_MS);
+          xSemaphoreGive(mutexImprime);
+        
+         xSemaphoreGive(semCustomers);
+         xSemaphoreGive(mutexLugares);
+      } else xSemaphoreGive(mutexLugares);
+    xSemaphoreGive(mutexLugares);
+  }
+
+
 }
 
 
